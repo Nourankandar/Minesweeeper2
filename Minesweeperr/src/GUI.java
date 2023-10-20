54ef399a8cf8c3ad641826956efb4da73462ae32
Nourankandar <147334965+Nourankandar@users.noreply.github.com>
2023-10-20 11:56:41 +0300

    aa

---
+++ Minesweeperr/src/GUI.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    PaneL paneL = new PaneL();
+    PaneL paneL1 = new PaneL();
+    PaneL paneL2 = new PaneL();
+
+    public GUI(){
+        setSize(618,620);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setTitle("MineSweeper");
+        setLocation(500, 100);
+        setLayout(null);
+        paneL.setBounds(2,100,600,480);
+        //setBounds(2,2,600,40)
+        //setBounds(2,42,600,98)
+
+        paneL.panel3();
+        paneL1.panel1(paneL1);
+        paneL2.panel2(paneL2);
+
+        add(paneL1);add(paneL2); add(paneL);
+
+        setVisible(true);
+
+    }
+}
---
+++ Minesweeperr/src/PaneL.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class PaneL extends JPanel {
+    public void panel1(JPanel p1){
+        p1.setLayout(new FlowLayout());
+        p1.setBounds(2,2,600,40);
+        p1.setBackground(new Color(0x7C8882));
+
+        JMenuBar bar =new JMenuBar();
+
+        JMenu Game =new JMenu("Game");
+        JMenuItem New =new JMenuItem("new");
+        JMenuItem Save =new JMenuItem("save");
+        JMenuItem Load =new JMenuItem("load");
+
+        Game.add(New);Game.add(Save);Game.add(Load);
+
+        JMenu Settings =new JMenu("Settings");
+        JMenuItem Time =new JMenuItem("time");
+        JMenuItem Player =new JMenuItem("player mode");
+
+        Settings.add(Time); Settings.add(Player);
+
+        bar.add(Game); bar.add(Settings);
+
+        p1.add(bar);
+
+
+
+
+    }
+
+    public void panel2(JPanel p2){
+        p2.setLayout(null);
+        p2.setBounds(2,44,600,55);
+        p2.setBackground(new Color(0xB27EFF));
+    }
+    Button button[][]= new Button[20][20];
+
+    public void panel3() {
+        setLayout(new GridLayout(12,12));
+        for(int i =0 ; i<12 ; i++){
+            for(int j=0;j<12;j++) {
+                button[i][j] = new Button();
+                add(button[i][j]);
+                setBackground(new Color(176, 211, 232));
+
+
+            }
+        }
+    }
+}
+
